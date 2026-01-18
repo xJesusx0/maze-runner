eea2ddc4563f9754ddb1d623a66696b9002a1cc5
jesus <snowcatyt1234@gmail.com>
2026-01-18 10:38:38 -0500

    feat: agregar multiples nodos vecinos

--- src/main/java/io/github/xjesusx0/Main.java
+++ src/main/java/io/github/xjesusx0/Main.java
@@ -6,25 +6,12 @@ public class Main {
     static void main() {
 
         Node start = new Node("Start");
-        Node first = new Node("First");
-        Node second = new Node("Second");
+        Node left = new Node("Left");
+        Node right = new Node("Right");
         Node finalNode = new Node(true, "Final");
 
-        start.setNext(first);
-        first.setNext(second);
-        second.setNext(finalNode);
-
-        Node current = start;
-        while (true) {
-            IO.println(current);
-
-            if(current.getIsFinal()){
-                IO.println("You are already final!");
-                return;
-            }
-
-            current = current.getNext();
-        }
-
+        start.connect(left, right);
+        left.connect(finalNode);
+        right.connect(finalNode);
     }
 }
--- src/main/java/io/github/xjesusx0/models/Node.java
+++ src/main/java/io/github/xjesusx0/models/Node.java
@@ -5,22 +5,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @AllArgsConstructor
 @Builder
 @Data
 public class Node {
     @ToString.Exclude
-    private Node next;
+    private List<Node> neighbors;
+
     private Boolean isFinal;
     private String name;
 
     public Node() {
         this.isFinal = false;
         this.name = "";
+        this.neighbors = new ArrayList<>();
     }
 
-    public Node(Node next) {
-        this.next = next;
+    public Node(Node... nodes) {
+        this.neighbors = new ArrayList<>(List.of(nodes));
         this.isFinal = false;
         this.name = "";
     }
@@ -28,15 +33,26 @@ public class Node {
     public Node(String name) {
         this.isFinal = false;
         this.name = name;
+        this.neighbors = new ArrayList<>();
     }
 
     public Node(boolean isFinal) {
         this.isFinal = isFinal;
         this.name = "";
+        this.neighbors = new ArrayList<>();
     }
 
     public Node(boolean isFinal, String name) {
         this.isFinal = isFinal;
         this.name = name;
+        this.neighbors = new ArrayList<>();
+    }
+
+    public void connect(Node node) {
+        this.neighbors.add(node);
+    }
+
+    public void connect(Node... nodes) {
+        this.neighbors.addAll(List.of(nodes));
     }
 }
